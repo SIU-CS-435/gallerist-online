02da821782ac0f091740f582ec7545b7d06b9ad3
Axel Wales <axelwales@gmail.com>
2016-02-16 16:21:23 -0600

    Added views for the game itself. Added 1 css class to Site.css. Changed the BundleConfig to use Site.css.

--- TeamJAMiN/TeamJAMiN/App_Start/BundleConfig.cs
+++ TeamJAMiN/TeamJAMiN/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace TeamJAMiN
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
