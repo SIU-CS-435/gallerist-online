4a5ce87611cf96289eaa278ee9e7461aa3e6cc6e
Michael Brajkovich <zerosauce@users.noreply.github.com>
2016-02-28 17:32:21 -0800

    add gameart and game artist

---
+++ TeamJAMiN/TeamJAMiN.GalleristGameEntities/GameArt.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamJAMiN.GalleristComponentEntities
+{
+    public class GameArt
+    {
+        public GameArt(TemplateArt temp)
+        {
+            Type = temp.Type;
+            Fame = temp.Fame;
+            NumTickets = temp.NumTickets;
+            FirstTicket = temp.FirstTicket;
+            SecondTicket = temp.SecondTicket;
+        }
+
+        public int Id { get; set; }
+        
+        public ArtType Type { get; set; }
+        public int Fame { get; set; }
+        public int NumTickets { get; set; }
+        public string FirstTicketData { get; set; }
+        public int Order { get; set; }
+        public VisitorTicketType[] FirstTicket
+        {
+            get
+            {
+
+                return Array.ConvertAll(FirstTicketData.Split(';'), v => (VisitorTicketType)Enum.Parse(typeof(VisitorTicketType), v));
+            }
+            set
+            {
+                FirstTicketData = String.Join(";", value.Select(v => v.ToString()).ToArray());
+            }
+        }
+        public string SecondTicketData { get; set; }
+        public VisitorTicketType[] SecondTicket
+        {
+            get
+            {
+
+                return Array.ConvertAll(SecondTicketData.Split(';'), v => (VisitorTicketType)Enum.Parse(typeof(VisitorTicketType), v));
+            }
+            set
+            {
+                SecondTicketData = String.Join(";", value.Select(v => v.ToString()).ToArray());
+            }
+        }
+    }
+}
---
+++ TeamJAMiN/TeamJAMiN.GalleristGameEntities/GameArtist.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamJAMiN.GalleristComponentEntities
+{
+    public class GameArtist
+    {
+        public int Id { get; set; }
+        public ArtistCategory Category { get; set; }
+        public ArtType ArtType { get; set; }
+        public int Fame { get; set; }
+        public int Promotion { get; set; }
+        public string StarLevelData { get; set; }
+        public int[] StarLevels
+        {
+            get
+            {
+                return Array.ConvertAll(StarLevelData.Split(';'), int.Parse);
+            }
+            set
+            {
+                StarLevelData = String.Join(";", value.Select(s => s.ToString()).ToArray());
+            }
+        }
+        public bool IsDiscovered { get; set; }
+        int AvailableArt { get; set; }
+    }
+}
