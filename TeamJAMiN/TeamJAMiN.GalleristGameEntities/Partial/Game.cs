b4ae7c318f1847406cdf366e77ed8f28d270c495
zerosauce <zerosauce@users.noreply.github.com>
2016-03-02 01:35:29 -0600

    Created a partial class for Game so that we can keep model data seperate from additional methods. This probably isnt necessary.

---
+++ TeamJAMiN/TeamJAMiN.GalleristGameEntities/Partial/Game.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamJAMiN.GalleristComponentEntities
+{
+    public partial class Game
+    {
+        public void AddArtStack(List<TemplateArt> artList)
+        {
+            var i = 0;
+            foreach (TemplateArt art in artList)
+            {
+                var gameArt = new GameArt(art);
+                gameArt.Order = i++;
+                Art.Add(gameArt);
+            }
+        }
+        public void AddArtStack(Dictionary<ArtType, List<TemplateArt>> artDict)
+        {
+            foreach (var artList in artDict.Values)
+            {
+                var i = 0;
+                foreach (TemplateArt art in artList)
+                {
+                    var gameArt = new GameArt(art);
+                    gameArt.Order = i++;
+                    Art.Add(gameArt);
+                }
+            }
+        }
+        public void AddArtists(List<TemplateArtist> artists)
+        {
+            foreach (TemplateArtist artist in artists)
+            {
+                var gameArtist = new GameArtist(artist);
+                Artists.Add(gameArtist);
+            }
+        }
+        public void AddReputationTiles(List<TemplateReputationTile> tiles)
+        {
+            foreach (TemplateReputationTile tile in tiles)
+            {
+                var gameTile = new GameReputationTile(tile);
+                ReputationTiles.Add(gameTile);
+            }
+        }
+        public void AddContracts(List<TemplateContract> contracts)
+        {
+            foreach (TemplateContract contract in contracts)
+            {
+                var gameContract = new GameContract(contract);
+                Contracts.Add(gameContract);
+            }
+        }
+    }
+}
