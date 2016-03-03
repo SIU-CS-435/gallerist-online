63b7a981f2cf3c977287199aa73915b19cb7f46c
Michael Brajkovich <zerosauce@users.noreply.github.com>
2016-03-02 21:58:36 -0600

    Updated game controller to not create a new game when going to Play Action. Also left out migrationy things.

--- TeamJAMiN/TeamJAMiN/Controllers/GameController.cs
+++ TeamJAMiN/TeamJAMiN/Controllers/GameController.cs
@@ -84,89 +84,28 @@ namespace TeamJAMiN.Controllers
         }
 
         /// <summary>
-        /// Takes you to the new game view. This also creates a game but we really need to do that in a post. We also need an additional step between clicking New Game
-        /// and creating the game. For example we probably want to setup rules, setup a game name etc.
+        /// Takes you to an existing view for a game
         /// </summary>
         /// <returns>Game View</returns>
         [Authorize]
         public ActionResult Play(int id = 0)
         {
-            if (id == 0)
+            using (var galleristContext = new GalleristComponentsDbContext())
             {
-                using (var galleristContext = new GalleristComponentsDbContext())
+                var game = galleristContext.Games.Include("Art").Include("Artists").Include("ReputationTiles").Include("Contracts").Include("Visitors").Include("Players").FirstOrDefault(m => m.Id == id);
+
+                if (game == null)
                 {
-                    Game newGame = new Game();
-                    newGame.CreateRandomSetup();
-
-                    //TODO: We need to allow users to input these values instead of auto generating
-                    var gameNameStrings = new List<String>{
-                        "The Gallerist Test Game 1",
-                        "Gallerist Game A",
-                        "Gallerist Game Dev",
-                        "Trying the gallerist!",
-                        "Let's give Gallerist a go!",
-                        "Gallerist - All welcome :)",
-                        "Gallerist time lets gooo...",
-                        "Gallerist Private Room"
-                    };
-
-                    var rand = new Random();
-                    var gameName = gameNameStrings.ElementAt(rand.Next(gameNameStrings.Count));
-                    newGame.Name = gameName;
-                    newGame.MaxNumberOfPlayers = 4;
-                    newGame.TurnLength = 60;
-
-                    //need standard for adding a player to a game
-                    using (var identityContext = new ApplicationDbContext())
-                    {
-                        //add me to the game
-                        newGame.Players.Add(new Player { UserId = identityContext.Users.First(m => m.UserName == User.Identity.Name).Id });
-                        galleristContext.Games.Add(newGame);
-                        galleristContext.SaveChanges();
-                        return View(newGame);
-                    }
+                    return View("NotFound");
                 }
-            }
-            else
-            {
-                //TODO: Make this less horrible. Need a new action
-                using (var galleristContext = new GalleristComponentsDbContext())
+
+                using (var identityContext = new ApplicationDbContext())
                 {
-                    var game = galleristContext.Games.Include("Art").Include("Artists").Include("ReputationTiles").Include("Contracts").Include("Visitors").FirstOrDefault(m => m.Id == id);
-
-                    if (game == null)
-                    {
-                        return View("NotFound");
-                    }
-
-                    using (var identityContext = new ApplicationDbContext())
-                    {
-                        //add me to the game
-                        ViewBag.userName = User.Identity.Name;
-                    }
-                    return View(game);
-                }                
+                    //add me to the game
+                    ViewBag.userName = User.Identity.Name;
+                }
+                return View(game);
             }
         }
-
-        /// <summary>
-        /// Takes you to the view for an existing game
-        /// </summary>
-        /// <returns>Specified Game View</returns>
-        //[Authorize]
-        //public ActionResult Play(int id)
-        //{
-        //    using (var galleristContext = new GalleristComponentsDbContext())
-        //    {
-        //        var game = galleristContext.Games.FirstOrDefault(m => m.Id == id);
-
-        //        if(game == null)
-        //        {
-        //            return View("NotFound");
-        //        }
-
-        //        return View(game);
-        //    }
-        //}
     }
 }
