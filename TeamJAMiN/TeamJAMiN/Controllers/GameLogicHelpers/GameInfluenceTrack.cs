d547ab0cb2ec5775660863f0b15dbfa45e52549b
Axel Wales <axelwales@gmail.com>
2016-03-20 18:46:45 -0500

    Added styling to the influence track.

---
+++ TeamJAMiN/TeamJAMiN/Controllers/GameLogicHelpers/GameInfluenceTrack.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamJAMiN.Controllers.GameLogicHelpers
+{
+    public static class GameInfluenceTrack
+    {
+        public static int[] InfluenceToMoney = { 0, 1, 4, 8, 12, 15, 18, 20, 22, 24, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35};
+    }
+}
