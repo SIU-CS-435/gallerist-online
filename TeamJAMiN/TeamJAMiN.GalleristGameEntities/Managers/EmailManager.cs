5cbcc4d57d155b4e025252cead0ce5d7e65f42c1
Michael Brajkovich <zerosauce@users.noreply.github.com>
2016-03-22 10:46:34 -0500

    added sendemail method

--- TeamJAMiN/TeamJAMiN.GalleristGameEntities/Managers/EmailManager.cs
+++ TeamJAMiN/TeamJAMiN.GalleristGameEntities/Managers/EmailManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,18 @@ namespace TeamJAMiN.GalleristComponentEntities.Managers
 {
     public class EmailManager
     {
+        public static void SendEmail(string body, string title, List<string> recipients)
+        {
+            var smtpClient = new SmtpClient();
+            var message = new MailMessage();
+            message.Body = body;
+            message.Subject = title;
+            foreach (var recipient in recipients)
+            {
+                message.To.Add(recipient);
+            }
+            smtpClient.Send(message);
+            //do email things
+        }
     }
 }
