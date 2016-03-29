bc7eba6f55d5308ea3d51ea8de6538892dcb845c
zerosauce <zerosauce@users.noreply.github.com>
2016-03-28 23:27:35 -0500

    Added async email sending and setup emails for new account registration. Could easily require email verification before registration now.

--- TeamJAMiN/TeamJAMiN.GalleristGameEntities/Managers/EmailManager.cs
+++ TeamJAMiN/TeamJAMiN.GalleristGameEntities/Managers/EmailManager.cs
@@ -1,22 +1,41 @@
 ﻿using System.Collections.Generic;
 using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace TeamJAMiN.GalleristComponentEntities.Managers
 {
     public class EmailManager
     {
-        public static void SendEmail(string title, string body, List<string> recipients)
+        public static void SendEmail(string subject, string body, List<string> recipients)
         {
             //do email things
-            var smtpClient = new SmtpClient();
             var message = new MailMessage();
             message.Body = body;
-            message.Subject = title;
+            message.Subject = subject;
             foreach (var recipient in recipients)
             {
                 message.To.Add(recipient);
             }
-            smtpClient.Send(message);
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.Send(message);
+            }
+        }
+
+        public static async Task SendEmailAsync(string subject, string body, List<string> recipients)
+        {
+            //do async email things
+            var message = new MailMessage();
+            message.Body = body;
+            message.Subject = subject;
+            foreach (var recipient in recipients)
+            {
+                message.To.Add(recipient);
+            }
+            using (var smtpClient = new SmtpClient())
+            {
+                await smtpClient.SendMailAsync(message);
+            }
         }
     }
 }
