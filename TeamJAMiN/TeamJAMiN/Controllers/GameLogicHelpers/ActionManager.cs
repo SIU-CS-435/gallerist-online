ed5fd1f0d2f3f2cb7c6a6a53547aa8a41049d920
Axel Wales <axelwales@gmail.com>
2016-03-28 21:05:48 -0500

    Added Player Setup

---
+++ TeamJAMiN/TeamJAMiN/Controllers/GameLogicHelpers/ActionManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TeamJAMiN.GalleristComponentEntities;
+
+namespace TeamJAMiN.Controllers.GameLogicHelpers
+{
+    public class ActionManager
+    {
+
+    }
+    public class LocationAction
+    {
+        public PlayerLocation ActionLocation { get; set; }
+    }
+    public enum ActionType
+    {
+
+    }
+}
