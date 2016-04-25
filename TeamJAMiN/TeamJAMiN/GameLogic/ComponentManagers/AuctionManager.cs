b3d97df19b131b6cc3197f3ff0fe28394ac5f6ec
Axel Wales <axelwales@gmail.com>
2016-04-25 08:19:09 -0500

    Added auction location view model

--- TeamJAMiN/TeamJAMiN/GameLogic/ComponentManagers/AuctionManager.cs
+++ TeamJAMiN/TeamJAMiN/GameLogic/ComponentManagers/AuctionManager.cs
@@ -15,9 +15,42 @@ namespace TeamJAMiN.Controllers.GameLogicHelpers
             { "TwoInfluence", 2 }
         };
 
+        public static Dictionary<string, int> PositioneByColumn = new Dictionary<string, int>()
+        {
+            { "OneInfluence", 3 },
+            { "ThreeInfluence", 1 },
+            { "TwoInfluence", 2 }
+        };
+
+        public static Dictionary<string, int> PositioneByRow = new Dictionary<string, int>()
+        {
+            { "Auction1", 0 },
+            { "Auction3", 1 },
+            { "Auction6", 2 }
+        };
+
+        public static Dictionary<PlayerAssistantLocation, BonusType> LocationToBonus = new Dictionary<PlayerAssistantLocation, BonusType>()
+        {
+            { PlayerAssistantLocation.Auction1, BonusType.ticket },
+            { PlayerAssistantLocation.Auction2, BonusType.assistant },
+            { PlayerAssistantLocation.Auction3, BonusType.twoTickets },
+            { PlayerAssistantLocation.Auction4, BonusType.assistant },
+            { PlayerAssistantLocation.Auction5, BonusType.influence },
+            { PlayerAssistantLocation.Auction6, BonusType.money },
+            { PlayerAssistantLocation.Auction7, BonusType.plazaVisitor },
+            { PlayerAssistantLocation.Auction8, BonusType.money },
+            { PlayerAssistantLocation.Auction9, BonusType.influence }
+        };
+
+
         public static string GetAuctionColumn(this Game game)
         {
             var currentLocation = game.CurrentTurn.CurrentAction.Location;
+            return GetAuctionColumnByLocationString(currentLocation);
+        }
+
+        public static string GetAuctionColumnByLocationString(string currentLocation)
+        {
             var locationParams = currentLocation.Split(':');
             return locationParams[1];
         }
@@ -25,6 +58,11 @@ namespace TeamJAMiN.Controllers.GameLogicHelpers
         public static string GetAuctionRow(this Game game)
         {
             var currentLocation = game.CurrentTurn.CurrentAction.Location;
+            return GetAuctionRowByLocationString(currentLocation);
+        }
+
+        public static string GetAuctionRowByLocationString(string currentLocation)
+        {
             var locationParams = currentLocation.Split(':');
             return locationParams[0];
         }
@@ -36,6 +74,33 @@ namespace TeamJAMiN.Controllers.GameLogicHelpers
                 return InfluenceByColumn[column];
             }
             return 0;
+        }
+
+        public static PlayerAssistantLocation GetAuctionLocation(this Game game)
+        {
+            var location = game.CurrentTurn.CurrentAction.Location;
+            return GetAuctionLocationByLocationString(location);
+        }
+
+        public static BonusType GetAuctionBonus(this Game game)
+        {
+            var location = game.CurrentTurn.CurrentAction.Location;
+            return GetBonusByAuctionLocationString(location);
+        }
+
+        public static PlayerAssistantLocation GetAuctionLocationByLocationString(string currentLocation)
+        {
+            var row = GetAuctionRowByLocationString(currentLocation);
+            var column = GetAuctionColumnByLocationString(currentLocation);
+            var auctionIndex = PositioneByColumn[column] + 3 * PositioneByRow[row];
+            var location = (PlayerAssistantLocation)Enum.Parse(typeof(PlayerAssistantLocation), "Auction" + auctionIndex);
+            return location;
+        }
+
+        public static BonusType GetBonusByAuctionLocationString(string location)
+        {
+            var assistantLocation = GetAuctionLocationByLocationString(location);
+            return LocationToBonus[assistantLocation];
         }
     }
 }
---
+++ TeamJAMiN/TeamJAMiN/Models/ComponentViewModels/AuctionLocationViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TeamJAMiN.Controllers.GameLogicHelpers;
+using TeamJAMiN.GalleristComponentEntities;
+using TeamJAMiN.Models.GameViewHelpers;
+
+namespace TeamJAMiN.Models.ComponentViewModels
+{
+    public class AuctionLocationViewModel
+    {
+        public AuctionLocationViewModel(string userName, Game game, string column, string row)
+        {
+            IsActivePlayer = FormHelper.IsActivePlayer(userName, game);
+            IsValidActionState = game.CurrentTurn.CurrentAction.State == GameActionState.InternationalMarket;
+
+            Game = game;
+            Column = column;
+            Row = row;
+            SetActionLocation(row, column);
+            SetAuctionClass(ActionLocation);
+        }
+
+        public Game Game { get; private set; }
+
+        public string ActionLocation { get; private set; }
+        public GameActionState State = GameActionState.Auction;
+
+        public bool IsActivePlayer { get; private set; }
+        public bool IsValidActionState { get; private set; }
+
+        private string Column { get; set; }
+        private string Row { get; set; }
+
+        public string AuctionClass { get; private set; }
+
+        private void SetActionLocation(string row, string col)
+        {
+            ActionLocation = row + ':' + col;
+        }
+
+        private void SetAuctionClass(string actionLocation)
+        {
+            var bonus = AuctionManager.GetBonusByAuctionLocationString(actionLocation);
+            AuctionClass = IconCss.BonusClass[bonus];
+        }
+    }
+}
