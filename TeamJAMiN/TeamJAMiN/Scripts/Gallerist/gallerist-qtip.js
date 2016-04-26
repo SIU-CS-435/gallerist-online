7450e8f227138b780ab4ce8c733eb604b69842a8
Michael Brajkovich <mbrajk@users.noreply.github.com>
2016-04-26 10:35:01 -0500

    added qtip functionality. Still needs work

---
+++ TeamJAMiN/TeamJAMiN/Scripts/Gallerist/gallerist-qtip.js
@@ -0,0 +1,8 @@
+﻿$('.qtip-basic').each(function () {
+    var tooltipString = $(this).attr('data-qtip-content');
+    $(this).qtip({
+        content: {
+            text: tooltipString
+        }
+    })
+});
