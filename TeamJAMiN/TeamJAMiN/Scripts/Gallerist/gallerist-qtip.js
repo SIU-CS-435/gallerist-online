4517d2d30bc3e16068bcbacbbcf9add0659f1ab3
Michael Brajkovich <mbrajk@users.noreply.github.com>
2016-04-26 20:57:28 -0500

    Fixed the default tooltips to appear under the mouse cursor and to disappear correctly

--- TeamJAMiN/TeamJAMiN/Scripts/Gallerist/gallerist-qtip.js
+++ TeamJAMiN/TeamJAMiN/Scripts/Gallerist/gallerist-qtip.js
@@ -1,8 +1,15 @@
-﻿$('.qtip-basic').each(function () {
-    var tooltipString = $(this).attr('data-qtip-content');
-    $(this).qtip({
-        content: {
-            text: tooltipString
+﻿$('[title!=""]').qtip({ //set all items with a non empty string as their title to use qtip
+    position: {
+        target: 'mouse',
+        adjust: {
+            mouse: false 
         }
-    })
-});
+    },
+    hide: {
+        fixed: true, //without this hovering on the tooltip itself causes problems with mouseout not closing the tooltip until you mouse back in
+        when: {
+            event: 'unfocus',
+            delay: 0            
+        },        
+    }
+}); 
