c7735fd547bf63022c197e893bc675e5dd6458f2
Michael Brajkovich <mbrajk@users.noreply.github.com>
2016-05-03 21:25:53 -0500

    formatted tooltips to be white with black text. We can further style them but default color did not match our site.

--- TeamJAMiN/TeamJAMiN/Scripts/Gallerist/gallerist-qtip.js
+++ TeamJAMiN/TeamJAMiN/Scripts/Gallerist/gallerist-qtip.js
@@ -1,15 +1,22 @@
 ﻿$('[title!=""]').qtip({ //set all items with a non empty string as their title to use qtip
     position: {
-        target: 'mouse',
+        my: 'top left',
+        at: 'bottom center',
+        //target: 'mouse',
         adjust: {
             mouse: false 
         }
     },
     hide: {
         fixed: true, //without this hovering on the tooltip itself causes problems with mouseout not closing the tooltip until you mouse back in
-        when: {
-            event: 'unfocus',
-            delay: 0            
-        },        
+        event: 'unfocus click mouseleave',
+        delay: 0,
+        leave: true
+    },
+    show: {
+        delay: 1000
+    },
+    style: {
+        classes: 'qtip-white qtip-shadow qtip-bootstrap'
     }
-}); 
+});
