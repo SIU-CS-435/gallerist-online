5f95bb3536aed49c265043f299f86c549ba28646
Michael Brajkovich <mbrajk@users.noreply.github.com>
2016-05-03 21:47:51 -0500

    Added a template tooltip for game board tutorials/help

--- TeamJAMiN/TeamJAMiN/Scripts/Gallerist/gallerist-qtip.js
+++ TeamJAMiN/TeamJAMiN/Scripts/Gallerist/gallerist-qtip.js
@@ -1,4 +1,5 @@
-﻿$('[title!=""]').qtip({ //set all items with a non empty string as their title to use qtip
+﻿//set all items with a non empty string as their title to use qtip
+$('[title!=""]').qtip({
     position: {
         my: 'top left',
         at: 'bottom center',
@@ -8,7 +9,7 @@
         }
     },
     hide: {
-        fixed: true, //without this hovering on the tooltip itself causes problems with mouseout not closing the tooltip until you mouse back in
+        fixed: false,
         event: 'unfocus click mouseleave',
         delay: 0,
         leave: true
@@ -18,5 +19,40 @@
     },
     style: {
         classes: 'qtip-white qtip-shadow qtip-bootstrap'
+    }
+});
+
+//this is a generic tooltip I am setting up that can be used for displaying help info on game board
+$('.board-information-section').qtip({
+    content:{
+        text: function (event, api) {
+            //we will ideally want to change how we pull the text and title but this is just a demo of how we can
+            //pull from data attribute of hover'd item
+            return $(this).attr('data-text');
+        },
+        title: function (event, api) {
+            //pull from title of hover'd item (this is the default behavior)
+            return $(this).attr('title');
+        }
+    },
+    position: {
+        my: 'center left',
+        at: 'center right',
+        target: 'mouse',
+        adjust: {
+            mouse: false
+        }
+    },
+    hide: {
+        fixed: true, //prevent from disappearing if you hover over it
+        event: 'unfocus mouseleave',
+        delay: 0,
+        leave: true
+    },
+    show: {
+        delay: 2000 //wait two seconds before showing help. We don't want to spam helps
+    },
+    style: {
+        classes: 'qtip-white qtip-shadow qtip-bootstrap' //might want to choose different styles for help tooltips
     }
 });
