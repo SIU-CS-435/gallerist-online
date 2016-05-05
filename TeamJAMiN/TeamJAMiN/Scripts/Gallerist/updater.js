28eb5c59aa15365ebf72979eeecbceb3099094ac
JBorders3010 <bordersjb@siu.edu>
2016-05-04 19:20:08 -0500

    fixed previous commit revert and added updater

---
+++ TeamJAMiN/TeamJAMiN/Scripts/Gallerist/updater.js
@@ -0,0 +1,6 @@
+﻿//JS File for updating html elements
+
+$(function (elementIdentifier, newElement) {    //identifier is element name, newElement is the string replacing info
+    var element = document.getElementById(elementIdentifier);
+    element = newElement;
+});
